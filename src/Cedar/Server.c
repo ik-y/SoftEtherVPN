fab6b3e9a1da10d9a03e0499b1f2c9c64027e09d
ikewai <91519064+ik-y@users.noreply.github.com>
2023-03-08 02:49:03 +0900

    Unlock server auth limitation

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10753,7 +10753,7 @@ bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 
 	if (StrCmpi(region, "JP") == 0 || StrCmpi(region, "CN") == 0)
 	{
-		ret = true;
+		ret = false;
 	}
 
 	return ret;
